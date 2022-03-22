3d5881b49af30b1eda6f9b8ee13506159a7d806d
siraz <siraznaorem07@gmail.com>
2022-03-22 07:58:37 +0530

    tree

---
+++ Tree Data Structure/Sum Root to Leaf Numbers.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) {
+ * val = x;
+ * left=null;
+ * right=null;
+ * }
+ * }
+ */
+
+public class Solution {
+
+    public void recursion(ArrayList<Integer> sum, ArrayList<Integer> currValue, TreeNode iter) {
+        // base condition 1
+        if (iter.left == null && iter.right == null) {
+            currValue.add(iter.val);
+            int num = 0;
+            for (int elem : currValue) {
+                num = ((num * 10) + elem) % 1003;
+            }
+            // System.out.println("Curr num formed "+num);
+            sum.set(0, (sum.get(0) + num) % 1003);
+            return;
+        }
+
+        // no base condition
+        currValue.add(iter.val);
+        if (iter.left != null)
+            recursion(sum, new ArrayList<Integer>(currValue), iter.left);
+        if (iter.right != null)
+            recursion(sum, new ArrayList<Integer>(currValue), iter.right);
+
+        return;
+    }
+
+    public int sumNumbers(TreeNode A) {
+        ArrayList<Integer> sum = new ArrayList<Integer>();
+        sum.add(0);
+        recursion(sum, new ArrayList<Integer>(), A);
+        return sum.get(0) % 1003;
+    }
+}
