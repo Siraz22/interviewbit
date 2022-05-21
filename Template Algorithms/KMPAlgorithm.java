b346053e4ce17f1fe410d2e1752734b039dc1e7a
siraz <siraznaorem07@gmail.com>
2022-05-21 13:57:00 +0530

    string algo

---
+++ Template Algorithms/KMPAlgorithm.java
@@ -0,0 +1,41 @@
+//GOAL : get index of the start and end index for maximum continuos subarray
+
+public class KMPAlgorithm {
+    public void algorithm(int[] arr) {
+
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        int newStartIndex = 0;
+        int startIndex = 0;
+        int endIndex = -1;
+
+        for (int i = 0; i < arr.length; ++i) {
+            currSum += arr[i];
+
+            if (currSum > max) {
+                max = currSum;
+
+                startIndex = newStartIndex;
+                endIndex = i;
+            }
+
+            if (currSum < 0) {
+                currSum = 0;
+
+                newStartIndex = i + 1; // ignore current num, causes sum to go < 0
+            }
+        }
+
+        System.out.println("start index = " + startIndex + ", end index = " + endIndex);
+        // output : start index = 3, end index = 6
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        KMPAlgorithm algo = new KMPAlgorithm();
+        algo.algorithm(arr);
+    }
+}
